b109a1894d575925f1ed93adda21a17ebe642edb
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-24 10:56:25 +0300

    Pelaaja ja tekoäly

---
+++ osa10-Osa10_05.PelaajaJaTekoaly/src/main/java/Pelaaja.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jukka
+ */
+public class Pelaaja {
+    private String nimi;
+
+    public void pelaa() {
+    }
+
+    public void tulostaNimi() {
+        System.out.println(this.nimi);
+    }
+}
---
+++ osa10-Osa10_05.PelaajaJaTekoaly/src/main/java/Tekoaly.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jukka
+ */
+public class Tekoaly extends Pelaaja {
+
+    public void lisaaSiirto() {
+    }
+
+    @Override
+    public void pelaa() {
+        super.pelaa(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
