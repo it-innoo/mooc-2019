29063355e2c03b69311b2e43a53a34f33bce39bd
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-29 21:04:43 +0300

    Maatilan lehmä

--- osa10-Osa10_14.Maatilasimulaattori/src/main/java/maatila/Main.java
+++ osa10-Osa10_14.Maatilasimulaattori/src/main/java/maatila/Main.java
@@ -5,5 +5,29 @@ public class Main {
 
     public static void main(String[] args) {
         // Testaa täällä ohjelmaasi
+        Lehma lehma = new Lehma();
+System.out.println(lehma);
+
+Eleleva elelevaLehma = lehma;
+elelevaLehma.eleleTunti();
+elelevaLehma.eleleTunti();
+elelevaLehma.eleleTunti();
+elelevaLehma.eleleTunti();
+
+System.out.println(lehma);
+
+Lypsava lypsavaLehma = lehma;
+lypsavaLehma.lypsa();
+
+System.out.println(lehma);
+System.out.println("");
+
+lehma = new Lehma("Ammu");
+System.out.println(lehma);
+lehma.eleleTunti();
+lehma.eleleTunti();
+System.out.println(lehma);
+lehma.lypsa();
+System.out.println(lehma);
     }
 }
