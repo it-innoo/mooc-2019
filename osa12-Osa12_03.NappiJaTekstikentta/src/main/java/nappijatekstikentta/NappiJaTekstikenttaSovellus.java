7cb4e426e4cbd4aafde00f74333a66bb1fc0baf7
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-29 11:54:06 +0300

    Osa12_04

--- osa12-Osa12_03.NappiJaTekstikentta/src/main/java/nappijatekstikentta/NappiJaTekstikenttaSovellus.java
+++ osa12-Osa12_03.NappiJaTekstikentta/src/main/java/nappijatekstikentta/NappiJaTekstikenttaSovellus.java
@@ -1,11 +1,36 @@
 package nappijatekstikentta;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class NappiJaTekstikenttaSovellus {
+public class NappiJaTekstikenttaSovellus extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NappiJaTekstikenttaSovellus.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Sovellukseni");
+        
+        Button nappikomponentti = new Button("Tämä on nappi");
+
+        TextField tekstikomponentti = new TextField("Tekstikenttä");
+
+        FlowPane komponenttiryhma = new FlowPane();
+        
+        komponenttiryhma.getChildren().add(nappikomponentti);
+        komponenttiryhma.getChildren().add(tekstikomponentti);
+
+        Scene nakyma = new Scene(komponenttiryhma);
+
+        stage.setScene(nakyma);
+        stage.show();
     }
 
 }
--- osa12-Osa12_04.BorderPane/src/main/java/borderpane/BorderPaneSovellus.java
+++ osa12-Osa12_04.BorderPane/src/main/java/borderpane/BorderPaneSovellus.java
@@ -1,11 +1,31 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class BorderPaneSovellus {
+public class BorderPaneSovellus extends Application  {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(BorderPaneSovellus.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane asettelu = new BorderPane();
+        asettelu.setTop(new Label("NORTH"));
+        asettelu.setRight(new Label("EAST"));
+        asettelu.setBottom(new Label("SOUTH"));
+
+        Scene nakyma = new Scene(asettelu);
+
+        stage.setScene(nakyma);
+        stage.show();
     }
 
 }
