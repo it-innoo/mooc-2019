c853b050d84a9a5e8f0433f389903a4d139ad125
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-29 11:22:16 +0300

    Osa12_02

--- osa12-Osa12_02.NappiJaTekstielementti/src/main/java/nappijatekstielementti/NappiJaTekstielementtiSovellus.java
+++ osa12-Osa12_02.NappiJaTekstielementti/src/main/java/nappijatekstielementti/NappiJaTekstielementtiSovellus.java
@@ -1,11 +1,36 @@
 package nappijatekstielementti;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class NappiJaTekstielementtiSovellus {
+public class NappiJaTekstielementtiSovellus extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NappiJaTekstielementtiSovellus.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Sovellukseni");
+        
+        Button nappikomponentti = new Button("Tämä on nappi");
+
+        Label tekstikomponentti = new Label("Tekstielementti");
+
+        FlowPane komponenttiryhma = new FlowPane();
+        
+        komponenttiryhma.getChildren().add(nappikomponentti);
+        komponenttiryhma.getChildren().add(tekstikomponentti);
+
+        Scene nakyma = new Scene(komponenttiryhma);
+
+        stage.setScene(nakyma);
+        stage.show();
     }
 
 }
