4e72595c3430f91bcef934872891a60e3eaa9283
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-31 14:46:36 +0300

    Osa12_09

--- osa12-Osa12_09.UseampiNakyma/src/main/java/sovellus/UseampiNakyma.java
+++ osa12-Osa12_09.UseampiNakyma/src/main/java/sovellus/UseampiNakyma.java
@@ -1,9 +1,58 @@
 package sovellus;
 
-public class UseampiNakyma {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class UseampiNakyma extends Application {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(args);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane ekaRuutu = new BorderPane();
+        Button ekaNappi = new Button("Tokaan näkymään!");
+        ekaRuutu.setTop(new Label("Eka näkymä!"));
+        ekaRuutu.setCenter(ekaNappi);
+
+        VBox tokaRuutu = new VBox();
+        tokaRuutu.setSpacing(20);
+        Button tokaNappi = new Button("Kolmanteen näkymään!");
+        tokaRuutu.getChildren().add(tokaNappi);
+        tokaRuutu.getChildren().add(new Label("Toka näkymä!"));
+
+        GridPane kolmasRuutu = new GridPane();
+        Button kolmasNappi = new Button("Ekaan näkymään!");
+        kolmasRuutu.add(new Label("Kolmas näkymä"), 0, 0);
+        kolmasRuutu.add(kolmasNappi, 1, 1);
+
+        Scene ekaNakyma = new Scene(ekaRuutu);
+        Scene tokaNakyma = new Scene(tokaRuutu);
+        Scene kolmasNakyma = new Scene(kolmasRuutu);
+
+        ekaNappi.setOnAction((event) -> {
+            stage.setScene(tokaNakyma);
+        });
+
+        tokaNappi.setOnAction((event) -> {
+            stage.setScene(kolmasNakyma);
+        });
+
+        kolmasNappi.setOnAction((event) -> {
+            stage.setScene(ekaNakyma);
+        });
+
+        stage.setTitle("Useampi Näkymä");
+        stage.setScene(ekaNakyma);
+        stage.show();
+    }
+ 
 }
